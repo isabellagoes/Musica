1d90759eb7ab741d5bbfc55d0816a111337bd798
Isabella Goes <isabellagoes.igs@gmail.com>
2023-09-11 00:04:01 -0300

    Correção da pasta github

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿Console.ForegroundColor = (ConsoleColor)13;
+Console.WriteLine("Decode - Paramore");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("How can I decide what's right?");
+Console.WriteLine("When you're clouding up my mind");
+Console.WriteLine("Can't win your losing fight all the time");
+Console.WriteLine("Not gonna ever own what's mine");
+Console.WriteLine("When you're always taking sides");
+Console.WriteLine("But you won't take away my pride");
+Console.WriteLine("No not this time");
+Console.WriteLine("Not this time");
+Console.WriteLine();
+
+Console.ForegroundColor = (ConsoleColor)13;
+Console.WriteLine("How did we get here?");
+Console.WriteLine("I used to know you so well");
+Console.WriteLine("How did we get here?");
+Console.WriteLine("Well, I think I know");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("The truth is hiding in your eyes");
+Console.WriteLine("And its hanging on your tongue");
+Console.WriteLine("Just boiling in my blood,");
+Console.WriteLine("But you think that I can't see");
+Console.WriteLine("What kind of man that you are");
+Console.WriteLine("If you're a man at all");
+Console.WriteLine("Well, I will figure this one out on my own");
+Console.WriteLine();
+
+Console.WriteLine("I'm screaming 'I love you so'");
+Console.WriteLine("But my thoughts you can't decode");
+Console.WriteLine();
+
+Console.ForegroundColor = (ConsoleColor)13;
+Console.WriteLine("How did we get here?");
+Console.WriteLine("I used to know you so well");
+Console.WriteLine("How did we get here?");
+Console.WriteLine("Well, I think I know");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Do you see what we've done?");
+Console.WriteLine("We're gonna make such fools of ourselves");
+Console.WriteLine("Do you see what we've done?");
+Console.WriteLine("We're gonna make such fools of ourselves");
+Console.WriteLine();
+
+Console.ForegroundColor = (ConsoleColor)13;
+Console.WriteLine("How did we get here?");
+Console.WriteLine("I used to know you so well");
+Console.WriteLine("How did we get here?");
+Console.WriteLine("Well, I used to know you so well");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("I think I know");
+Console.WriteLine("I think I know");
+Console.WriteLine("There is something that I see in you");
+Console.WriteLine("It might kill me");
+Console.WriteLine("I want it to be true");
